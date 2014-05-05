2d86b37a1f5d820bdb724c4fc29fc103b8da5ff1
Elvis Oliveira <elvis.olv@gmail.com>
2014-05-05 00:22:27 -0300

    finally lxml and urllib are working together!

--- hello.py
+++ hello.py
@@ -1,21 +1,17 @@
-import cherrypy
-import json
 import urllib
-import html5lib
+import cherrypy
+import lxml.html
 
 class HelloWorld(object):
-	def index(self):
-		a_query = Query()
-		text = a_query.search()
-		return text
-	index.exposed = True
+    def index(self):
+        url = 'http://www.9gag.com/'
+        data = urllib.urlopen(url).read()
+        document = lxml.html.fromstring(data)
+        elements = document.cssselect('article img')
+        for element in elements:
+            print lxml.html.tostring(element)
+        return "hello"
+    index.exposed = True
 
-class Query():
-	def search(self):
-		url = 'http://www.google.com/'
-		data = urllib.urlopen(url)
-		# document = html5lib.parse(data)
-		return data
-
-cherrypy.config.update({'server.socket_host': '127.0.0.1', 'server.socket_port': 9999 })
+cherrypy.config.update({'server.socket_host': '127.0.0.1', 'server.socket_port': 9999})
 cherrypy.quickstart(HelloWorld())
